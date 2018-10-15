843000ed889b0ff230f5a5d6358d3ec2587b3231
xoriuz <ficky.irwanto@gmail.com>
2018-10-15 23:03:28 +0700

    - initial working commit

--- index.js
+++ index.js
@@ -1,29 +1,116 @@
-const fs = require("fs"); 
-const Path = require ("path");
+const Path = require('path');
+var fs = require('fs');
+var express = require('express');
 const sharp = require('sharp');
+var mime = require('mime-types');
 
 
-module.exports = function (req, res, next) {
-    
-    // config
-    var filename = '';
-    var format = 'png';
-    var width = 100;
-    var height = 100;
+module.exports = function(basePath) {
+    var router = express.Router();
 
-    // resizing
-    const readStream = fs.createReadStream(filename);
-    var transform = sharp();
+    if (!fs.existsSync(basePath)) fs.mkdirSync(basePath);
 
-    if (format) {
-        transform = transform.toFormat(format);
+    var cachePath = Path.join(basePath,'cache_bz_image');
+    if (!fs.existsSync(cachePath)) fs.mkdirSync(cachePath);
+
+
+    var resizeImage = async function(fileid, width, height, fitStrategy="inside", fnPath) {
+        var prefix = [];
+        if (width) prefix.push('w'+width);
+        else prefix.push('wauto');
+
+        if (height) prefix.push('h'+height);
+        else prefix.push('hauto');
+
+        if (fitStrategy) prefix.push('f'+fitStrategy);
+        else prefix.push('fauto');
+        
+        prefix.push(fileid);
+
+        var cacheFilename = prefix.join('_');
+
+        try {
+            var filename = fnPath(fileid);
+            var resizedFilename = fnPath(cacheFilename, true);
+
+            if (fs.existsSync(resizedFilename)) {
+                // console.log("return cache");
+                return Promise.resolve(resizedFilename);
+            }
+            else {
+                return sharp(filename)
+                .resize(width*1, height*1, {fit:fitStrategy})
+                .toFile(resizedFilename)
+                .then((data)=> {
+                    return resizedFilename;
+                })
+            }
+
+            
+        } catch (error) {
+            return Promise.reject(error);
+        }
+        
     }
-    if (width || height) {
-        transform = transform.resize(width, height);
-    }
+    
 
+    router.use((req, res, next)=> {
+        // register helper
+        req._bz_getImagePath = function(filename, isCache=false) {
+            if (!filename) {
+                if (isCache) 
+                    return cachePath;
+                else
+                    return basePath;
+            }
+
+
+            if (isCache) 
+                return cachePath + Path.sep + filename
+            else
+                return basePath  + Path.sep + filename
+        };
+        next();
+    })
 
     
-    res.type(`image/${format || 'png'}`);
-    readStream.pipe(transform).pipe(res);
+    router.get('/clear-cache', (req, res, next)=> {
+    
+        var directory = req._bz_getImagePath(null, true);
+        fs.readdir(directory, (err, files) => {
+            if (err) throw err;
+        
+            for (const file of files) {
+                fs.unlink(Path.join(directory, file), err => {
+                    if (err) throw err;
+                });
+            }
+
+            res.send("OK");
+        });
+    
+    });
+    
+    
+    router.get('/:width?.:height?.:fitStrategy?/:fileid', async (req, res, next)=> {
+        try {
+            var f;
+            if (!req.params.width && !req.params.height && !req.params.fitStrategy) {
+                f = req._bz_getImagePath(req.params.fileid);
+            }
+            else {
+                f = await resizeImage(req.params.fileid, req.params.width, req.params.height, req.params.fitStrategy, req._bz_getImagePath);
+            }
+            var img = fs.readFileSync(f);
+            var contentType = mime.contentType(img);
+            res.writeHead(200, {'Content-Type': contentType });
+            res.end(img, 'binary');
+        } catch (error) {
+            console.log("ERROR", error);
+            next(error);
+        }
+    
+    });
+
+    return router;
 }
---
+++ readme.md
@@ -0,0 +1,42 @@
+# bz-mailer
+
+## Install
+`npm install bz-image -S`
+
+## Usage Example
+
+### Register Route
+
+```javascript
+const bzImage = require('bz-image');
+var imageDirectory = path.join(__dirname,'_uploads')
+app.use('/images', bzImage(imageDirectory));
+```
+
+### Routes
+**/images/width.height.fitStrategy/filename**
+
+
+width ( optional )
+target image width
+
+height ( optional ) target image height
+
+fitStrategy ( optional )
+if width and height specified, fitStrategy can be used with values:
+- contain
+- cover
+- inside
+- outside
+- fill
+
+
+#### Examples
+Fit Width
+`/images/200/image1.jpg`
+
+Fit Height
+`/images/0.200/image1.jpg`
+
+Fit Both with Fith Strategy = Contain
+`/images/200.200.contain/image1.jpg`
